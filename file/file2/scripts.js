8736f19ac3f9b813730d48b4c163c6fe4b7330c0
Edyta <styrczulaedyta89@gmail.com>
2021-10-06 19:12:55 +0200

    Building blocks

--- file/file2/scripts.js
+++ file/file2/scripts.js
@@ -1,1 +1,43 @@
 console.log("This is JavaScript");
+
+let bookCount = 10;
+
+for (let i = 0; i < bookCount; i++) {
+    console.log(i)
+}
+
+for (let i = 1; i <= bookCount; i++) {
+    console.log(i)
+}
+let book = 21;
+for (let i = 0; i < book; i++) {
+    console.log(i)
+}let printNumbersTill=[];
+ let numbers=20
+for (let i = 1; i <= numbers; i++) {
+   printNumbersTill.push(i)
+    }
+    console.log(printNumbersTill)
+let printNumbersTilll=[];
+ let numberss=15
+for (let i = 1; i <= numberss; i++) {
+   printNumbersTilll.push(i)
+    }
+    console.log(printNumbersTilll)
+
+  
+ let getGreetingTo= "Hello"  
+ let name = " Mark"          
+console.log(getGreetingTo + " " + name )
+
+
+let printValues = [0,3,6,7,9]
+  for (let i = 0; i < printValues.length; i++) {
+    console.log(printValues[i]);
+  }
+
+  console.log("forEach")
+  let myNumbers = [10, 20, 30, 50, 12];
+  myNumbers.forEach(n=>{
+    console.log(n);
+  })
