2650206a90254a69801203ae2575172fa74ac694
Edyta <styrczulaedyta89@gmail.com>
2021-10-06 14:14:30 +0200

    correct

---
+++ file/scripts.js
@@ -0,0 +1,56 @@
+let age=32;
+undefined
+age
+32
+let ww2=6;
+undefined
+ww2
+6
+let boolean = true
+undefined
+boolean
+true
+boolean
+true
+let secondsOfDay = 24*60*60
+undefined
+secondsOfDay
+86400
+let hungarians = 10000*100/7753000000
+undefined
+hungarians
+0.00012898232942086933
+let percentage = "percentage";
+undefined
+hungarians+percentage
+'0.00012898232942086933percentage'
+ let participants=654/7;
+let myString = 'This is a string';
+undefined
+let name="Edyta";
+undefined
+let greeting="Hello";
+undefined
+greeting + ""+ name
+'HelloEdyta'
+greeting+" " + name
+'Hello Edyta'
+name.length
+let names = ["Anna","Richard","Tom"]
+names.length
+names.push("Alex")
+names.length
+names[0]="Edyta"
+if(names[2].length>5){console.log("yes")}
+if(names.length>4){console.log("yes more then 4 items")}
+
+let color={
+   colors =[ "red","white","black"] ,
+   hasManyColors,
+    }
+    if(colors.length > 3){hasManyColors=true} 
+    else{colors.length <= 3}{hasManyColors=false}
+
+
+
+ 
